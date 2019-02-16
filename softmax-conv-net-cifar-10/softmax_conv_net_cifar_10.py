2f5d4a4310ebbec695dc1050c9c1aa18c416fd16
Napow <nathanpowell1990@gmail.com>
2019-02-16 17:16:42 +0800

    Tweak project structure, introduce barebones gui
    Further change to structure needed to manage models as objects rather than single global variable
    Prediction function should display image and prediction result in separate window

--- softmax-conv-net-cifar-10/softmax_conv_net_cifar_10.py
+++ softmax-conv-net-cifar-10/softmax_conv_net_cifar_10.py
@@ -1,4 +1,6 @@
 import numpy as np
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
 from keras.callbacks import EarlyStopping
 from keras.datasets import cifar10
 from keras.models import Sequential, load_model
@@ -10,72 +12,89 @@ from keras.layers.pooling import MaxPooling2D
 from keras.preprocessing import image
 from keras.utils import to_categorical
 
-if __name__ == '__main__':
+# Network classification category titles
+category = ['airplane','car','bird','cat','deer',
+                'dog','frog','horse','ship','truck']
+model = None
 
-    running = True
+# Opens file browser to choose image to be classified,
+# returns predicted category title
+def predict():
+    img_path = askopenfilename() # Opens file browser
+    # Image is loaded as 32x32, converted to 32x32x3 array,
+    # then expanded to fit models 4 dimension requirement,
+    # pred = one-hot encoded prediction
+    img = image.load_img(img_path, target_size=(32,32)) 
+    test_image = image.img_to_array(img) 
+    test_image = np.expand_dims(test_image, axis=0) 
+    pred = model.predict(test_image, batch_size=1)
+    cat_title = category[np.argmax(pred)]
+    print('Image predicted to be ' + cat_title)
+# Opens file browser to choose current model
+def loadmodel():
+    model_path = askopenfilename() # Opens file browser
+    try:
+        return load_model(model_path)
+    except:
+        print('Error loading model')
 
-    while running:
+def savemodel():
+    save_path = askopenfilename() # Opens file browser
+    try:
+        model.save(save_path)
+    except:
+        print('Error saving model')
 
-        action = input('Input: ').lower()
+# Creates a new model, TODO: separate and modularise
+def newmodel():
 
-        if action.startswith('loadmodel'):
+    model = Sequential()
 
-            model = load_model('softmax_conv_net_cifar_10_model_' + action.split(' ')[1] + '.h5')
+    model.add(BatchNormalization())
 
-        elif action.startswith('newmodel'):
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3))) # 30, 30, 32
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu')) # 28, 28, 64
+    model.add(Dropout(0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2))) # 14, 14, 64
 
-            model = Sequential()
-            model.add(BatchNormalization())
-            model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3))) # 30, 30, 32
-            model.add(Conv2D(64, kernel_size=(3, 3), activation='relu')) # 28, 28, 64
-            model.add(Dropout(0.1))
-            model.add(MaxPooling2D(pool_size=(2, 2))) # 14, 14, 64
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu')) # 12, 12, 128
+    model.add(MaxPooling2D(pool_size=(2, 2))) # 6, 6, 128
+    model.add(Dropout(0.2))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu')) # 4, 4, 128
+    model.add(MaxPooling2D(pool_size=(2, 2))) # 2, 2, 128
 
-            model.add(Conv2D(128, kernel_size=(3, 3), activation='relu')) # 12, 12, 128
-            model.add(MaxPooling2D(pool_size=(2, 2))) # 6, 6, 128
-            model.add(Dropout(0.2))
-            model.add(Conv2D(128, kernel_size=(3, 3), activation='relu')) # 4, 4, 128
-            model.add(MaxPooling2D(pool_size=(2, 2))) # 2, 2, 128
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
 
-            model.add(Flatten())
-            model.add(Dense(1024, activation='relu'))
-            model.add(Dense(10, activation='softmax'))
+# Loads training data, compiles the model, runs 5x epochs of 
+# 64 size minibatches of funn 50k training set (CIFAR-10)
+def trainmodel():
+    (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+    model.compile(loss='categorical_crossentropy',
+                    optimizer=Adam(lr=0.0001, decay=1e-6),
+                    metrics=['accuracy'])
+    model.fit(X_train / 255.0, to_categorical(Y_train),
+                batch_size=64,
+                shuffle=True,
+                epochs=5,
+                validation_data=(X_test / 255.0, to_categorical(Y_test)),
+                callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
+    model.summary()
+    scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
+    print('Loss: %.3f' % scores[0])
+    print('Accuracy: %.3f' % scores[1])
 
-        elif action.startswith('loaddata'):
+main_window = tk.Tk()
+main_window.title('softmax-conf-net-cifar-10.py')
 
-            (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+top_frame = tk.Frame(main_window).pack()
+bottom_frame = tk.Frame(main_window).pack(side = "bottom")
 
-        elif action.startswith('compile'):
+load_btn = tk.Button(bottom_frame, text='Load CNN', command=loadmodel).pack(side='left')
+train_btn = tk.Button(bottom_frame, text='Train', command=trainmodel).pack(side='left')
+predict_btn = tk.Button(bottom_frame, text='Predict', command=predict).pack(side='left')
+save_btn = tk.Button(bottom_frame, text='Save CNN', command=savemodel).pack(side='right')
 
-            model.compile(loss='categorical_crossentropy',
-                          optimizer=Adam(lr=0.0001, decay=1e-6),
-                          metrics=['accuracy'])
-
-        elif action.startswith('train'):
-
-            model.fit(X_train / 255.0, to_categorical(Y_train),
-                      batch_size=64,
-                      shuffle=True,
-                      epochs=5,
-                      validation_data=(X_test / 255.0, to_categorical(Y_test)),
-                      callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
-            model.summary()
-            scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
-            print('Loss: %.3f' % scores[0])
-            print('Accuracy: %.3f' % scores[1])
-
-        elif action.startswith('savemodel'):
-
-            model.save('softmax_conv_net_cifar_10_model_' + action.split(' ')[1] + '.h5')
-
-        elif action.startswith('predict'):
-            category = ['airplane','car','bird','cat','deer','dog','frog','horse','ship','truck']
-            img_path = action.split(' ')[1]
-            img = image.load_img(img_path, target_size=(32,32))
-            test_image = image.img_to_array(img)
-            test_image = np.expand_dims(test_image, axis=0)
-            pred = model.predict(test_image,batch_size=1)
-            for i in range(0, len(category)):
-                if pred[0][i]:
-                    print(category[i])
-                    break
+model = loadmodel()
+main_window.mainloop()
